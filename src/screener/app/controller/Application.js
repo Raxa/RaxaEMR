0cac5d67210638684c482fa7d501176f37de0c5d
John Stoecker <johnstoecker@gmail.com>
2013-01-16 16:31:03 +0530

    -changing date time to work with patient queues

--- src/screener/app/controller/Application.js
+++ src/screener/app/controller/Application.js
@@ -277,18 +277,18 @@ Ext.define("Screener.controller.Application", {
         var list_regEncounter = Ext.create('Screener.model.PostList', {
             name: "Registration Encounter",
             description: "Patients encountered Registration" + "startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d),
-            searchQuery: "?encounterType=" + localStorage.regUuidencountertype + "&startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d)
+            searchQuery: "?encounterType=" + localStorage.regUuidencountertype + "&startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d, -6)
         });
         var list_scrEncounter = Ext.create('Screener.model.PostList', {
             name: "Screener Encounter",
             description: "Patients encountered Screener on " + "startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d),
-            searchQuery: "?encounterType=" + localStorage.screenerUuidencountertype + "&startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d)
+            searchQuery: "?encounterType=" + localStorage.screenerUuidencountertype + "&startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d, -6)
 
         });
         var list_outEncounter = Ext.create('Screener.model.PostList', {
             name: "Outpatient Encounter",
             description: "Patients encountered Outpatient on " + "startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d),
-            searchQuery: "?encounterType=" + localStorage.outUuidencountertype + "&startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d)
+            searchQuery: "?encounterType=" + localStorage.outUuidencountertype + "&startDate=" + Util.Datetime(d, 24) + "&endDate=" + Util.Datetime(d, -6)
 
         });
         numberOfStoresWritten = 0;
