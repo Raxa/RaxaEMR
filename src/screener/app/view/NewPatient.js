b0c5f4104bbeae5a87a0163766caf855237467c5
madanpiyush <madanpiyush@gmail.com>
2013-02-05 12:17:50 +0530

    opdLite: NewPatient styled

--- src/screener/app/view/NewPatient.js
+++ src/screener/app/view/NewPatient.js
@@ -2,6 +2,20 @@
  * This panel will overlay when the new patient button is
  * pressed. This is a form that gets user name and shows
   */
+
+var NEW_PATIENT_CONSTANTS = {
+    FIELD_BORDER: 'border-color: #333333; border-width: 1px; border-style : solid;',
+    FIELD_WIDTH: 300,
+    LABEL_WIDTH: 135,
+    LABEL_STYLE: 'background-color: #eeeeee;',
+    CONTAINER_LAYOUT: { 
+                        type: 'hbox',
+                        align: 'strech'
+                    },
+    CONTAINER_MARGIN: '5 0 5 0',
+    RADIO_FIELD_WIDTH: 230
+}
+
 Ext.define("Screener.view.NewPatient", {
     requires: ['Ext.field.Text', 'Ext.field.Number'],
     extend: 'Ext.form.Panel',
@@ -13,7 +27,7 @@ Ext.define("Screener.view.NewPatient", {
         hidden: true,
         // Set the width and height of the panel
         width: 500,
-        height: 390,
+        height: 500,
         floating: true,
         //centered: true,
         showAnimation: {
@@ -41,34 +55,75 @@ Ext.define("Screener.view.NewPatient", {
                 ui: 'decline',
             }]
         },{
+            xtype: 'container',
+            layout: NEW_PATIENT_CONSTANTS.CONTAINER_LAYOUT, 
+            margin: NEW_PATIENT_CONSTANTS.CONTAINER_MARGIN,                       
+            items: [{
+                xtype: 'container',
+                width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH,
+                items: [{
+                    xtype: 'textfield',
+                    clearIcon: false,
+                    style: NEW_PATIENT_CONSTANTS.LABEL_STYLE ,
+                    value: 'First Name',
+                    width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH
+                    }]
+            },
+            {
             xtype: 'textfield',
             id: 'givenName',
             name: 'givenname',
-            label: 'First Name', // Ext.i18n.appBundle.getMsg('RaxaEmrScreener.view.NewPatient.giv_name')
-            margin: '5 0 5 0'
+            width: NEW_PATIENT_CONSTANTS.FIELD_WIDTH,
+            style: NEW_PATIENT_CONSTANTS.FIELD_BORDER
+            }]
         }, {
+            xtype: 'container',
+            layout : NEW_PATIENT_CONSTANTS.CONTAINER_LAYOUT,
+            margin: NEW_PATIENT_CONSTANTS.CONTAINER_MARGIN,     
+           items: [{
+                xtype: 'container',
+                width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH,
+                items: [{
+                    xtype: 'textfield',
+                    clearIcon: false,
+                    style: NEW_PATIENT_CONSTANTS.LABEL_STYLE ,
+                    value: 'Last Name',
+                    width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH
+                    }]
+            },
+            {
             xtype: 'textfield',
             id: 'familyName',
             name: 'familyname',
-            label: 'Last Name', // Ext.i18n.appBundle.getMsg('RaxaEmrScreener.view.NewPatient.fam_name')
-            margin: '5 0 5 0'
-        }, 
-        {
+            width: NEW_PATIENT_CONSTANTS.FIELD_WIDTH,
+            style: NEW_PATIENT_CONSTANTS.FIELD_BORDER
+            }]
+        },  {
+            xtype: 'container',
+            layout : NEW_PATIENT_CONSTANTS.CONTAINER_LAYOUT,
+            margin: NEW_PATIENT_CONSTANTS.CONTAINER_MARGIN,     
+            items: [{
+                xtype: 'container',
+                width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH,
+                items: [{
+                    xtype: 'textfield',
+                    clearIcon: false,
+                    style: NEW_PATIENT_CONSTANTS.LABEL_STYLE ,
+                    value: 'Age',
+                    width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH
+                    }]
+            },
+                    {
             xtype  : 'container',
             id: 'ageDateOfBirth',
-//            layout : {
-//                type  : 'hbox'
-//            },
             items : [
             {
                 xtype: 'numberfield',
                 id: 'patientAge',
                 name: 'patientAge',
-//                labelWidth: 138,
-                label: 'Age', // Ext.i18n.appBundle.getMsg('RaxaEmrScreener.view.NewPatient.age_dob'),
+                width: NEW_PATIENT_CONSTANTS.FIELD_WIDTH,
+                style: NEW_PATIENT_CONSTANTS.FIELD_BORDER,
                 allowDecimals: false,
-//                placeHolder: 'Age',
-                margin: '5 0 5 1',
                 listeners: {
                     blur: function(field) {
                         field.setValue(Math.floor(field.getValue()));
@@ -76,35 +131,44 @@ Ext.define("Screener.view.NewPatient", {
                         {
                             Ext.Msg.alert('Wrong Input','Patient Age should be between '+ Util.OPEN_MRS_MIN_AGE +' and '+ Util.OPEN_MRS_MAX_AGE);
                             field.setValue('');
-
                         }
                     }
                 }
-            },{
-                xtype: 'textfield',
-                id: 'dob',
-                name: 'dob',
-                placeHolder: 'YYYY-M-D',
-//                width: 172,
-                hidden: true
-            }
-            ]
+            }]
+        }]
         },
         {
+            xtype: 'container',            
+            layout : NEW_PATIENT_CONSTANTS.CONTAINER_LAYOUT,
+            margin: NEW_PATIENT_CONSTANTS.CONTAINER_MARGIN,
+           items: [{
+                xtype: 'container',
+                width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH,
+                items: [{
+                    xtype: 'textfield',
+                    clearIcon: false,
+                    style: NEW_PATIENT_CONSTANTS.LABEL_STYLE ,
+                    value: 'Gender',
+                    width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH
+                }]
+            },
+            {
             xtype  : 'container',
             id: 'sexRadioGroup',
+            style: NEW_PATIENT_CONSTANTS.FIELD_BORDER,
             layout : {
-                type  : 'hbox',
+                type  : 'vbox',
                 align : 'strech'
-            },
-            margin: '5 0 5 1',
+            },  
             items  : [
             {
                 xtype : 'radiofield',
                 label : 'Male', // Ext.i18n.appBundle.getMsg('RaxaEmrScreener.view.NewPatient.male'),
                 value: 'M',
                 name  : 'choice',
-                labelWidth: 70,
+                style: NEW_PATIENT_CONSTANTS.LABEL_STYLE,
+                labelAlign : 'right',
+                labelWidth: NEW_PATIENT_CONSTANTS.RADIO_FIELD_WIDTH,
                 flex  : 1
             },
             {
@@ -112,7 +176,9 @@ Ext.define("Screener.view.NewPatient", {
                 label : 'Female', // Ext.i18n.appBundle.getMsg('RaxaEmrScreener.view.NewPatient.female'),
                 value: 'F',
                 name  : 'choice',
-                labelWidth: 90,
+                labelAlign : 'right',
+                labelWidth: NEW_PATIENT_CONSTANTS.RADIO_FIELD_WIDTH,
+                style: NEW_PATIENT_CONSTANTS.LABEL_STYLE,
                 flex  : 1
             },
             {
@@ -120,15 +186,35 @@ Ext.define("Screener.view.NewPatient", {
                 label : 'Other', // Ext.i18n.appBundle.getMsg('RaxaEmrScreener.view.NewPatient.other'),
                 value: 'O',
                 name  : 'choice',
-                labelWidth: 70,
+                labelAlign : 'right',
+                labelWidth: NEW_PATIENT_CONSTANTS.RADIO_FIELD_WIDTH,
+                style: NEW_PATIENT_CONSTANTS.LABEL_STYLE,
                 flex  : 1
             }
             ]
-        },{
+            }
+            ]
+        }, {
+            xtype: 'container',
+            layout : NEW_PATIENT_CONSTANTS.CONTAINER_LAYOUT,
+            margin: NEW_PATIENT_CONSTANTS.CONTAINER_MARGIN,     
+           items: [{
+                xtype: 'container',
+                width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH,
+                items: [{
+                    xtype: 'textfield',
+                    clearIcon: false,
+                    style: NEW_PATIENT_CONSTANTS.LABEL_STYLE ,
+                    value: 'Contact No',
+                    width: NEW_PATIENT_CONSTANTS.LABEL_WIDTH
+                    }]
+            },
+            {
             xtype: 'textfield',
             id: 'contactNumber',
-            label: 'Contact Number',
-            margin: '5 0 5 0'
+            width: NEW_PATIENT_CONSTANTS.FIELD_WIDTH,
+            style: NEW_PATIENT_CONSTANTS.FIELD_BORDER
+            }]
         },{
             xtype: 'button',
             id: 'savePatientButton',
